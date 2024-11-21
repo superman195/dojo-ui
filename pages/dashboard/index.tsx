b5adb2b45ded9cd09e37596675eca06c5b8519cc
v2img <v2img@tensorplex.ai>
2024-11-21 11:22:40 +0800

    fixed dashboard page to use layout

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,11 +2,11 @@
 import MainFooter from '@/components/Common/Footer/MainFooter';
 import SubscriptionModal from '@/components/Common/Modal/SubscriptionModal';
 import DashboardGraphAndMetrics from '@/components/DashboardPageComponents/DashboardGraphAndMetrics';
-import DashboardHeader from '@/components/DashboardPageComponents/DashboardHeader';
 import LeaderboardSection from '@/components/DashboardPageComponents/LeaderboardSection';
 import { WalletManagement } from '@/components/TaskListPageComponents';
 import { useModal } from '@/hooks/useModal';
 import useSubnetMetagraph from '@/hooks/useSubnetMetaGraph';
+import Layout from '@/layout';
 import { NonRootNeuronObj } from '@/types/DashboardTypes';
 import { MODAL } from '@/types/ProvidersTypes';
 import { GetServerSideProps } from 'next';
@@ -52,26 +52,8 @@ const DashboardPage: React.FC<Props> = ({ delegates }) => {
   const { openModal } = useModal(MODAL.wallet);
   const { address } = useAccount();
 
-  const jwtTokenKey = `${process.env.NEXT_PUBLIC_REACT_APP_ENVIRONMENT}__jwtToken`;
-  useEffect(() => {
-    const handleStorageChange = (event: StorageEvent) => {
-      if (event.key === 'wagmi.io.metamask.disconnected' || event.key === jwtTokenKey) {
-        window.location.reload();
-      }
-    };
-
-    window.addEventListener('storage', handleStorageChange);
-    return () => window.removeEventListener('storage', handleStorageChange);
-  }, [jwtTokenKey]);
-
   return (
-    <div
-      className="min-h-screen text-black"
-      style={{
-        background: 'linear-gradient(to bottom, #FFFFF4 0%, #E1F5F4 54%, #FEFFF5 100%)',
-      }}
-    >
-      <DashboardHeader setShowUserCard={setShowUserCard} />
+    <Layout isFullWidth headerText={'Dashboard'}>
       <main className="mx-5 mt-5 max-w-4xl lg:mx-auto xl:mx-auto">
         <DashboardGraphAndMetrics subnetData={subnetData} loading={loading} error={error} />
         <LeaderboardSection miners={minerData} validators={validatorData} loading={loading} error={error} />
@@ -87,7 +69,7 @@ const DashboardPage: React.FC<Props> = ({ delegates }) => {
       )}
       {isModalVisible && <SubscriptionModal setIsModalVisible={setIsModalVisible} isModalVisible={isModalVisible} />}
       <MainFooter />
-    </div>
+    </Layout>
   );
 };
 
