6aa0b9565dd52df0dbd7fd4ce6f44a3064eb0235
theRed-Coder <theRedCoder@tensorplex.ai>
2024-12-18 14:32:31 +0700

    removed unnecessary padding that was breaking the UI of the DashboardGraphAndMetrics

--- components/DashboardPageComponents/DashboardGraphAndMetrics/index.tsx
+++ components/DashboardPageComponents/DashboardGraphAndMetrics/index.tsx
@@ -247,7 +247,7 @@ function DashboardGraphAndMetrics({ subnetData, loading, error }: DashboardGraph
   // Add resize handler
 
   return (
-    <div className="container mx-auto mb-6 px-0 lg:px-8">
+    <div className="container mx-auto mb-6 px-0">
       <div className="flex w-full flex-col justify-between gap-3 lg:flex-row">
         <div className="flex flex-row gap-3 overflow-x-auto py-3 lg:min-w-[240px] lg:flex-col lg:overflow-x-visible lg:p-0">
           {kpiMetrics.map((metric, index) => (
