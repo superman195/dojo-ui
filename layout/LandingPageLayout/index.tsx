d9ff2553315dba4d4bf60d47e5d9c6d48380930f
v2img <v2img@tensorplex.ai>
2025-03-19 22:33:29 +0800

    modfied title to put in correct position

--- layout/LandingPageLayout/index.tsx
+++ layout/LandingPageLayout/index.tsx
@@ -1,15 +1,21 @@
 import MainFooter from '@/components/Common/Footer/MainFooter';
 import NavigationBar from '@/components/Common/NavigationBar';
+import Head from 'next/head';
 import { ReactNode } from 'react';
 
 const LandingPageLayout = ({ children }: { children: ReactNode }) => (
-  <div className="min-h-screen bg-[#FFFFF4] text-black">
-    <div className="border-b-2 border-black bg-[#F6F6E6] text-white">
-      <NavigationBar openModal={() => {}} isHomePage />
+  <>
+    <Head>
+      <title>Dojo - Improve Decentralized AI Model with Crowd Sourcing</title>
+    </Head>
+    <div className="min-h-screen bg-[#FFFFF4] text-black">
+      <div className="border-b-2 border-black bg-[#F6F6E6] text-white">
+        <NavigationBar openModal={() => {}} isHomePage />
+      </div>
+      <main className="mx-auto">{children}</main>
+      <MainFooter />
     </div>
-    <main className="mx-auto">{children}</main>
-    <MainFooter />
-  </div>
+  </>
 );
 
 export default LandingPageLayout;
